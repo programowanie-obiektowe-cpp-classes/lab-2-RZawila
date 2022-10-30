d779a5945e930cbb3655708ba8c7a4ae577519e4
RZawila <ryszaw46@gmail.com>
2022-10-30 14:13:28 +0100

    Pierwsza wersja do sprawdzenia

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -4,5 +4,19 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+public:
+    ResourceManager(){}; // konstruktor
+    ~ResourceManager() // destruktor?
+    {
+    delete A;//?
+    }
+    ResourceManager(const ResourceManager& rm1) = default;// konstruktor kopiujacy
+    ResourceManager(const ResourceManager&& rm2) = default;// konstruktor przenosz¹cy
+    double get()
+    {
+        return A.get();
+    }
+
+private:
+    Resource() A;// classa ktora ma zarzadzac
 };
