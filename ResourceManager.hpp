dcc1bc4fae6f000ece6efc954c0fbb078b64083d
RZawila <ryszaw46@gmail.com>
2022-10-30 15:15:18 +0100

    wersja druga- u mnie działa na kompie chyba?

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -5,15 +5,45 @@
 class ResourceManager
 {
 public:
-    ResourceManager(){}; // konstruktor
-    ~ResourceManager(){}; // destruktor?
-    ResourceManager(const ResourceManager& rm1) = default;// konstruktor kopiujacy
-    ResourceManager(ResourceManager&& rm2) = default;// konstruktor przenosz¹cy
+    ResourceManager()
+    {
+        A = new Resource;
+    }; // konstruktor
+
+    ~ResourceManager()
+    {
+        delete A;
+    }; // destruktor?
+
+    ResourceManager(const ResourceManager& rm1) = default;// konstruktor kopiujacy, defaultowy jak w intsrukcji
+
+    ResourceManager& operator=(const ResourceManager& rm2)
+    {
+        if(&rm2 != this)
+        {
+            delete A;
+            A = new Resource{*rm2.A};
+        }
+        return *this;
+    }
+    ResourceManager(ResourceManager&& rm3)// konstruktor przenosz¹cy
+    {
+        A = rm3.A;
+        rm3.A = nullptr;
+    }
+
+    ResourceManager& operator=(ResourceManager&& rm4)// konstruktor przenosz¹cy
+    {
+        delete A;
+        A = rm4.A;
+        rm4.A = nullptr;
+        return *this;
+    }
     double get()
     {
-        return A.get();
+        return A->get();
     };
 
 private:
-    Resource A;// classa ktora ma zarzadzac
+    Resource* A;// classa ktora ma zarzadzac
 };
